e692bbf575ffbc3bcf1330c2486c30bf024ba0c3
Sam Stavinoha <samuel.stavinoha@rackspace.com>
2014-09-10 01:01:36 +0000

    dependencies

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
+REQUIRES = [
+    'python-swiftclient>=2.2.0',
+    'futures>=2.1.6',
+]
+
 setup(name='mrzero',
       version='0.0.1',
       url='https://github.com/samstav/mrzero',
+      install_requires=REQUIRES,
       py_modules=['mrzero'],
       scripts=['mrzero.py'],
      )
